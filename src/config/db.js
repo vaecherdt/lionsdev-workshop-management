f95939708aca1345131640282ad250036931006d
vaecherdt <coup-aquatic-0e@icloud.com>
2024-10-14 17:07:25 -0300

    remote database implementation

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const connectDB = async () => {
     await mongoose
-    .connect(`mongodb://localhost:27017/vehicle-workshop`)
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
 };
